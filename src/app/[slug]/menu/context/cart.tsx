12e11afbf134633f34bc29a8af327e3feece1f46
Renan Muller <renanmuller@hotmail.com.br>
2025-04-14 22:09:56 -0300

    feat: ensure same product can be added more the once to cart

--- src/app/[slug]/menu/context/cart.tsx
+++ src/app/[slug]/menu/context/cart.tsx
@@ -31,7 +31,24 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   };
 
   const addProduct = (product: CartProduct) => {
-    setProducts((prev) => [...prev, product]);
+    const productIsAlrearyOnTheCart = products.some(
+      (prevProduct) => prevProduct.id === product.id,
+    );
+    if (!productIsAlrearyOnTheCart) {
+      return setProducts((prev) => [...prev, product]);
+    }
+
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id === product.id) {
+          return {
+            ...prevProduct,
+            quantity: prevProduct.quantity + product.quantity,
+          };
+        }
+        return prevProduct;
+      });
+    });
   };
 
   return (
