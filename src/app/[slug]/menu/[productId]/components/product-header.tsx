ba61cbcfbca8bfe4e90e278bcac49d8df3a44d5b
Renan Muller <renanmuller@hotmail.com.br>
2025-04-18 18:16:19 -0300

    feat: ensure all orders and back buttons work as expected

--- src/app/[slug]/menu/[productId]/components/product-header.tsx
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -3,7 +3,7 @@
 import { Product } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 
@@ -12,9 +12,13 @@ interface ProductHeaderProps {
 }
 
 const ProductHeader = ({ product }: ProductHeaderProps) => {
+  const { slug } = useParams<{ slug: string }>();
   const router = useRouter();
   const handleBackClick = () => {
     router.back();
+  };
+  const handleOrderClick = () => {
+    router.push(`/${slug}/orders`);
   };
   return (
     <div className="relative min-h-[300px] w-full">
@@ -33,6 +37,7 @@ const ProductHeader = ({ product }: ProductHeaderProps) => {
         className="object-contain"
       />
       <Button
+        onClick={handleOrderClick}
         variant={"secondary"}
         size={"icon"}
         className="absolute right-4 top-4 z-50 rounded-full"
